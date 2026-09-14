97de7751683146aa5432d7a969ee4c4fbcc1f552
craigmoloneyg <craig.moloneyg@gmail.com>
2026-09-14 13:18:34 +1000

    Load routed section pages in Price 2 Plate

--- cloudflare/square-worker.mjs
+++ cloudflare/square-worker.mjs
@@ -7,7 +7,7 @@ export default {
     const response=await legacy.fetch(request,env,ctx);
     const type=response.headers.get('content-type')||'';
     if(request.method==='GET'&&type.includes('text/html')){
-      return new HTMLRewriter().on('body',{element(e){e.append('<script src="/square-ui.js" defer></script>',{html:true});}}).transform(response);
+      return new HTMLRewriter().on('body',{element(e){e.append('<script src="/square-ui.js" defer></script><script src="/page-router.js" defer></script>',{html:true});}}).transform(response);
     }
     return response;
   }
